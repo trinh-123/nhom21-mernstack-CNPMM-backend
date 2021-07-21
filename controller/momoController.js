b17fdbacb336759980c7da3a3c47bce8fa15970c
Ly Tien <lytienking@gmail.com>
2021-07-22 00:44:45 +0700

    momo7

--- controller/momoController.js
+++ controller/momoController.js
@@ -80,44 +80,77 @@ module.exports.getData= async(req,res)=>{
 module.exports.createOrder=async(req,res)=>{
     // let cart= await Cart.findOne({userID:req.body.userId});
     const _amount= req.body.totalPrice;
-    const orderID=req.body.listOrderId
-    const embed_data = `userId=${req.body.userId}`;
-    let order={
-                partnerCode: config.parnerCode,
-                requestId: uuidv4(),
-                accessKey:config.accessKey,
-                orderId: orderID, // translation missing: vi.docs.shared.sample_code.comments.app_trans_id
-                returnUrl: "https://kltn-tnshop.vercel.app/", // miliseconds
-                extraData: embed_data,
-                notifyUrl: config.notifyUrl,
-                amount: (_amount*1000).toString(),
-                orderInfo: `TN - Payment for the orders #${req.body.listOrderID}`,
-                requestType: "captureMoMoWallet",
-                lang: "en",
-
-            }
-            const data = `partnerCode=${order.partnerCode}&accessKey=${order.accessKey}&requestId=${order.requestId}&amount=${order.amount}&orderId=${order.orderId}&orderInfo=${order.orderInfo}&returnUrl=${order.returnUrl}&notifyUrl=${order.notifyUrl}&extraData=${order.extraData}`;
-            
-            order.signature = crypto.createHmac("sha256",config.secretKey).update(data).digest("hex");
-            let result={};
-            try {
-            let postConfig = {
-                method: 'post',
-                url: config.endpoint,
-                headers: {
-                    'Content-Type': 'application/json; charset=UTF-8'
-                },
-                data: order
-            };
-            
-            axios(postConfig)
-                .then(function (response) {
-                    res.json({order:order,data:response.data})
-                })
-                .catch(function (error) {
-                });
-            } catch (error) {
-            res.json(error)
-            }
+    const orderId=req.body.listOrderId;
+    const orderID=req.body.listOrderId.split(",")
+    let postConfig;
+    let order;
+    let data;
+    let embed_data;
+    if(orderID==undefined){
+      embed_data = `userId=${req.body.userId}`;
+      order={
+                  partnerCode: config.parnerCode,
+                  requestId: uuidv4(),
+                  accessKey:config.accessKey,
+                  orderId: orderId, // translation missing: vi.docs.shared.sample_code.comments.app_trans_id
+                  returnUrl: "https://kltn-tnshop.vercel.app/", // miliseconds
+                  extraData: embed_data,
+                  notifyUrl: config.notifyUrl,
+                  amount: (_amount*1000).toString(),
+                  orderInfo: `TN - Payment for the orders #${req.body.listOrderID}`,
+                  requestType: "captureMoMoWallet",
+                  lang: "en",
+  
+              }
+              data = `partnerCode=${order.partnerCode}&accessKey=${order.accessKey}&requestId=${order.requestId}&amount=${order.amount}&orderId=${order.orderId}&orderInfo=${order.orderInfo}&returnUrl=${order.returnUrl}&notifyUrl=${order.notifyUrl}&extraData=${order.extraData}`;
+              
+              order.signature = crypto.createHmac("sha256",config.secretKey).update(data).digest("hex");        
+    }else{
+      let listId;
+      for(var i=0;i<orderID.length;i++){
+        if(i=orderID.length-1){
+          listId=orderID[i]+listId
+        }else{
+          listId=listId+"-"+orderID[i]
+        }
+      }
+      embed_data = `userId=${req.body.userId}`;
+      order={
+                  partnerCode: config.parnerCode,
+                  requestId: uuidv4(),
+                  accessKey:config.accessKey,
+                  orderId: listId, // translation missing: vi.docs.shared.sample_code.comments.app_trans_id
+                  returnUrl: "https://kltn-tnshop.vercel.app/", // miliseconds
+                  extraData: embed_data,
+                  notifyUrl: config.notifyUrl,
+                  amount: (_amount*1000).toString(),
+                  orderInfo: `TN - Payment for the orders #${req.body.listOrderID}`,
+                  requestType: "captureMoMoWallet",
+                  lang: "en",
+  
+              }
+              data = `partnerCode=${order.partnerCode}&accessKey=${order.accessKey}&requestId=${order.requestId}&amount=${order.amount}&orderId=${order.orderId}&orderInfo=${order.orderInfo}&returnUrl=${order.returnUrl}&notifyUrl=${order.notifyUrl}&extraData=${order.extraData}`;
+              
+              order.signature = crypto.createHmac("sha256",config.secretKey).update(data).digest("hex"); 
+    }
+    try {
+      postConfig = {
+          method: 'post',
+          url: config.endpoint,
+          headers: {
+              'Content-Type': 'application/json; charset=UTF-8'
+          },
+          data: order
+      };
+      
+      axios(postConfig)
+          .then(function (response) {
+              res.json({order:order,data:response.data})
+          })
+          .catch(function (error) {
+          });
+      } catch (error) {
+      res.json(error)
+      }
 
 }
